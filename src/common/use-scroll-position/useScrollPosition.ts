2b07e6c8e58046eb67e4819c275055e1c1cdeef5
Shane Yu <shanesimpkins@gmail.com>
2020-06-04 17:29:43 +0100

    refactor: rename use scroll position file name
    relates to #2

---
+++ src/common/use-scroll-position/useScrollPosition.ts
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react';
+
+interface ScrollPosition {
+  x: number;
+  y: number;
+}
+
+const getScrollPosition = (): ScrollPosition =>
+  window ? { x: window.pageXOffset, y: window.pageYOffset } : { x: 0, y: 0 };
+
+const useScrollPosition = (): ScrollPosition => {
+  const [position, setPosition] = useState<ScrollPosition>(getScrollPosition());
+
+  useEffect(() => {
+    let animationFrameRequestRunning: number | null = null;
+
+    const handleScrollEvent = () => {
+      if (window && animationFrameRequestRunning === null) {
+        animationFrameRequestRunning = window.requestAnimationFrame(() => {
+          setPosition(getScrollPosition());
+          animationFrameRequestRunning = null;
+        });
+      }
+    };
+
+    if (window) {
+      window.addEventListener('scroll', handleScrollEvent);
+
+      return () => window.removeEventListener('scroll', handleScrollEvent);
+    }
+  }, []);
+
+  return position;
+};
+
+export default useScrollPosition;
